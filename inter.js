8a1870fd5db040e95097379e0a34b108a5e3eef9
Luiz Arthur <142501091+Luiznito@users.noreply.github.com>
2025-09-28 00:33:22 -0300

    Add files via upload

---
+++ inter.js
@@ -0,0 +1,8 @@
+function FecharInfo(){
+    const inf = document.getElementById('infoEmp');
+    inf.style.display = 'none';
+}
+function AbrirInf(){
+    const inf = document.getElementById('infoEmp');
+    inf.style.display = 'inline';
+}
